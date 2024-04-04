082c36db262b44b2ed2fccbbe03f885bbca37069
Saimon Islam Prantik <82695288+SaimonIslamPrantik@users.noreply.github.com>
2024-04-04 20:35:46 +0600

    NEW MOBILE LAYOUT
    Optimised for mobile views

---
+++ redirect.js
@@ -0,0 +1,5 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // Redirect to mobile.html if the user is on a mobile device
+    window.location.href = "mobile.html";
+};
+
